dad283488ac17089415dac7475555944f6e65c74
shomarzzz <67146629+shomarzzz@users.noreply.github.com>
2021-08-25 16:16:55 +0600

    Update complex.py

--- complex.py
+++ complex.py
@@ -17,7 +17,7 @@ print(img.shape)
 @numba.jit(nopython=True)
 def imgtocomp(new, a, b):
     def func(z):
-        return z**(2)
+        return np.log(z)
     center = [a, b]
     a = len(img)
     b = len(img[0])
